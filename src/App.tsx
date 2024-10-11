bd049ca08281a9761152aad343234140aa7ab23e
gumrahsindar <gsindar@gmail.com>
2024-10-11 21:14:23 +0300

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import './App.scss'
+
+function App() {
+  return (
+    <main className='form'>
+      <div className='form__container'>
+        <h1 className='heading'>Contact Us</h1>
+      </div>
+    </main>
+  )
+}
+
+export default App
