85dab8d7547b02c2e0d911c5eed5ae93442687f2
gumrahsindar <gsindar@gmail.com>
2024-10-14 14:14:39 +0300

    refactor: Improve form validation and aria attrs for accesibility

--- src/App.tsx
+++ src/App.tsx
@@ -32,7 +32,7 @@ function App() {
 
   return (
     <main>
-      <form noValidate onSubmit={handleSubmit(onSubmit)} className='form'>
+      <form onSubmit={handleSubmit(onSubmit)} className='form'>
         <div className='form__container'>
           <h1 className='form__header heading'>Contact Us</h1>
           <div className='form__layout'>
@@ -41,7 +41,6 @@ function App() {
                 <div>
                   <label htmlFor='first-name'>
                     First Name <span aria-hidden>*</span>
-                    <span className='sr-only'>Required</span>
                   </label>
                   <input
                     className={errors.firstName ? 'error-input' : ''}
@@ -53,13 +52,19 @@ function App() {
                     })}
                     type='text'
                     id='first-name'
+                    autoComplete='given-name'
                     aria-invalid={errors.firstName ? 'true' : 'false'}
                     aria-describedby={
                       errors.firstName ? 'first-name-error' : ''
                     }
+                    aria-required
                   />
                   {errors.firstName && (
-                    <p role='alert' id='first-name-error' className='error'>
+                    <p
+                      aria-live='assertive'
+                      id='first-name-error'
+                      className='error'
+                    >
                       {errors.firstName.message}
                     </p>
                   )}
@@ -67,7 +72,6 @@ function App() {
                 <div>
                   <label htmlFor='last-name'>
                     Last Name <span aria-hidden>*</span>
-                    <span className='sr-only'>Required</span>
                   </label>
                   <input
                     className={errors.lastName ? 'error-input' : ''}
@@ -79,11 +83,17 @@ function App() {
                     })}
                     type='text'
                     id='last-name'
+                    autoComplete='family-name'
                     aria-invalid={errors.lastName ? 'true' : 'false'}
                     aria-describedby={errors.lastName ? 'last-name-error' : ''}
+                    aria-required
                   />
                   {errors.lastName && (
-                    <p role='alert' id='last-name-error' className='error'>
+                    <p
+                      aria-live='assertive'
+                      id='last-name-error'
+                      className='error'
+                    >
                       {errors.lastName.message}
                     </p>
                   )}
@@ -93,7 +103,6 @@ function App() {
             <div className='form-line'>
               <label htmlFor='email'>
                 Email <span aria-hidden>*</span>
-                <span className='sr-only'>Required</span>
               </label>
               <input
                 className={errors.email ? 'error-input' : ''}
@@ -109,21 +118,25 @@ function App() {
                 })}
                 type='email'
                 id='email'
+                autoComplete='email'
                 aria-invalid={errors.email ? 'true' : 'false'}
                 aria-describedby={errors.email ? 'email-error' : ''}
+                aria-required
               />
               {errors.email && (
-                <p role='alert' id='email-error' className='error'>
+                <p aria-live='assertive' id='email-error' className='error'>
                   {errors.email.message}
                 </p>
               )}
             </div>
             <div className='form-line'>
-              <fieldset>
+              <fieldset aria-required='true'>
                 <legend>
                   Query Type <span aria-hidden>*</span>
-                  <span className='sr-only'>Required</span>
                 </legend>
+                <span className='sr-only' id='query-type-required'>
+                  Please select a query type. This field is required.
+                </span>
                 <div className='form__query-type'>
                   <label
                     htmlFor='general-enquiry'
@@ -141,7 +154,9 @@ function App() {
                       id='general-enquiry'
                       aria-invalid={errors.queryType ? 'true' : 'false'}
                       aria-describedby={
-                        errors.queryType ? 'query-type-error' : ''
+                        errors.queryType
+                          ? 'query-type-error query-type-required'
+                          : 'query-type-required'
                       }
                     />
                     General Enquiry
@@ -162,14 +177,20 @@ function App() {
                       id='support-request'
                       aria-invalid={errors.queryType ? 'true' : 'false'}
                       aria-describedby={
-                        errors.queryType ? 'query-type-error' : ''
+                        errors.queryType
+                          ? 'query-type-error query-type-required'
+                          : 'query-type-required'
                       }
                     />
                     Support Request
                   </label>
                 </div>
                 {errors.queryType && (
-                  <p role='alert' id='query-type-error' className='error'>
+                  <p
+                    aria-live='assertive'
+                    id='query-type-error'
+                    className='error'
+                  >
                     {errors.queryType.message}
                   </p>
                 )}
@@ -178,7 +199,6 @@ function App() {
             <div className='form-line'>
               <label htmlFor='message'>
                 Message <span aria-hidden>*</span>
-                <span className='sr-only'>Required</span>
               </label>
               <textarea
                 className={errors.message ? 'error-input' : ''}
@@ -191,9 +211,10 @@ function App() {
                 id='message'
                 aria-invalid={errors.message ? 'true' : 'false'}
                 aria-describedby={errors.message ? 'message-error' : ''}
+                aria-required
               ></textarea>
               {errors.message && (
-                <p role='alert' id='message-error' className='error'>
+                <p aria-live='assertive' id='message-error' className='error'>
                   {errors.message.message}
                 </p>
               )}
@@ -212,12 +233,13 @@ function App() {
                   id='consent'
                   aria-invalid={errors.consent ? 'true' : 'false'}
                   aria-describedby={errors.consent ? 'consent-error' : ''}
+                  aria-required
                 />
                 I hereby consent to being contacted by the team
                 <span aria-hidden>*</span>
               </label>
               {errors.consent && (
-                <p role='alert' id='consent-error' className='error'>
+                <p aria-live='assertive' id='consent-error' className='error'>
                   {errors.consent.message}
                 </p>
               )}
